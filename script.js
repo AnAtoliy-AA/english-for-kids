1c6fbf9fe57292d2476703bc4d6eed58f4b66f63
AnAtoliy_AK <aliaksandrau89@gmail.com>
2020-04-18 19:57:39 +0300

    fix: menu drop down

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ import playTrainSwitcher from './src/play-train-switcher';
 function runEnglishForKidsApp() {
   const container = new CardsContainer();
 
-  
+
   container.generateDomCardContainer();
-  // document.querySelectorAll('.open-close-btn').addEventListener('click', (event) => {
-  //   event.preventDefault();
-  //   document.querySelectorAll('.overlay').classList.toggle('overlay-open');
-  //   document.getElementById('hamburger-icon').classList.toggle('hamburger-open');
-  // });
+  document.querySelector('.open-close-btn').addEventListener('click', (event) => {
+    event.preventDefault();
+    document.querySelector('.overlay').classList.toggle('overlay-open');
+    document.getElementById('hamburger-icon').classList.toggle('hamburger-open');
+  });
+
   playTrainSwitcher();
 }
 
