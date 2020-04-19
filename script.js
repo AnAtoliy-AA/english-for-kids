4e0cf676b0638c6c2a35e51804ea34ea01877294
AnAtoliy_AK <aliaksandrau89@gmail.com>
2020-04-19 14:34:41 +0300

    refactor: create header module

--- script.js
+++ script.js
@@ -1,49 +1,14 @@
 import CardsContainer from './src/cards-container';
 import playTrainSwitcher from './src/play-train-switcher';
+import mainMenu from './src/header';
+
 
 function runEnglishForKidsApp() {
   const container = new CardsContainer();
 
-
   container.generateDomCardContainer();
-  document.querySelector('.open-close-btn').addEventListener('click', (event) => {
-    event.preventDefault();
-    document.querySelector('.overlay').classList.toggle('overlay-open');
-    document.getElementById('hamburger-icon').classList.toggle('hamburger-open');
-    // document.querySelector('.overlay-content').addEventListener('blur', () => {
-    //   document.querySelector('.overlay').classList.toggle('overlay-open');
-    //   document.getElementById('hamburger-icon').classList.toggle('hamburger-open');
-    // });
-  });
-
-  const mainMenuCategoryLink = document.querySelector('.overlay-content').querySelectorAll('a');
-
-  mainMenuCategoryLink.forEach((el) => {
-    el.addEventListener('click', (event) => {
-      const id = event.target.innerHTML;
-
-      if (id === 'Main menu') {
-        //  console.log('xcvvvvvvvvvvvvvvv');
-        let startPage = container.getMainPage();
-        container.applyPageToDOM(startPage);
-        // container.createDomTitleCard();
-      } else {
-        container.openCardCategoryPage(id);
-      }
-      // mainMenuCategoryLink.
-      mainMenuCategoryLink.forEach((el) => {
-        el.classList.remove('main-menu-link_active');
-      });
-      event.target.classList.add('main-menu-link_active');
-      document.querySelector('.overlay').classList.toggle('overlay-open');
-      document.getElementById('hamburger-icon').classList.toggle('hamburger-open');
-    })
-  })
-
-
-
+   mainMenu(container);
   playTrainSwitcher();
-
 }
 
 document.body.onload = runEnglishForKidsApp;
--- src/header.js
+++ src/header.js
@@ -1,39 +1,35 @@
-function createHeader() {
-    // document.querySelector('.open-close-btn').addEventListener('click', (event) => {
-    //     event.preventDefault();
-    //     document.querySelector('.overlay').classList.toggle('overlay-open');
-    //     document.getElementById('hamburger-icon').classList.toggle('hamburger-open');
-    //     // document.querySelector('.overlay-content').addEventListener('blur', () => {
-    //     //   document.querySelector('.overlay').classList.toggle('overlay-open');
-    //     //   document.getElementById('hamburger-icon').classList.toggle('hamburger-open');
-    //     // });
-    //   });
+function mainMenu(container) {
+  
+ 
+    document.querySelector('.open-close-btn').addEventListener('click', (event) => {
+        event.preventDefault();
+        document.querySelector('.overlay').classList.toggle('overlay-open');
+        document.getElementById('hamburger-icon').classList.toggle('hamburger-open');
+      });
     
-    //   const mainMenuCategoryLink = document.querySelector('.overlay-content').querySelectorAll('a');
+      const mainMenuCategoryLink = document.querySelector('.overlay-content').querySelectorAll('a');
     
-    //   mainMenuCategoryLink.forEach((el) => {
-    //     el.addEventListener('click', (event) => {
-    //       const id = event.target.innerHTML;
+      mainMenuCategoryLink.forEach((el) => {
+        el.addEventListener('click', (event) => {
+          const id = event.target.innerHTML;
     
-    //       if (id === 'Main menu') {
-    //         //  console.log('xcvvvvvvvvvvvvvvv');
-    //         let startPage = container.getMainPage();
-    //         container.applyPageToDOM(startPage);
-    //         // container.createDomTitleCard();
-    //       } else {
-    //         container.openCardCategoryPage(id);
-    //       }
-    //       // mainMenuCategoryLink.
-    //       mainMenuCategoryLink.forEach((el) => {
-    //         el.classList.remove('main-menu-link_active');
-    //       });
-    //       event.target.classList.add('main-menu-link_active');
-    //       document.querySelector('.overlay').classList.toggle('overlay-open');
-    //       document.getElementById('hamburger-icon').classList.toggle('hamburger-open');
-    //     })
-    //   })
+          if (id === 'Main menu') {
+            let startPage = container.getMainPage();
+            container.applyPageToDOM(startPage);
+          } else {
+            container.openCardCategoryPage(id);
+          }
+          mainMenuCategoryLink.forEach((el) => {
+            el.classList.remove('main-menu-link_active');
+          });
+          event.target.classList.add('main-menu-link_active');
+          document.querySelector('.overlay').classList.toggle('overlay-open');
+          document.getElementById('hamburger-icon').classList.toggle('hamburger-open');
+        })
+      })
+    
     
     
 }
 
-export default createHeader;
+export default mainMenu;
